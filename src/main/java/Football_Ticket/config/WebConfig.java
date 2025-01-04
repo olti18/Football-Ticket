56585c68f1f20f3d73e866d2c6a48e2621e52adc
Olti Berisha <olti.berisha.150@gmail.com>
2025-01-04 23:51:05 +0100

    Stripe Config File

---
+++ src/main/java/Football_Ticket/config/WebConfig.java
@@ -0,0 +1,18 @@
+package Football_Ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")  // Allow your frontend to access the backend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
